104b08c673b30b2bb8687d2af38b886281972124
Aminreza Sefid <aminreza_sefid@yahoo.com>
2024-06-28 16:59:24 +0330

    objects updated

--- image_generation/Simple_2Object_Generation.py
+++ image_generation/Simple_2Object_Generation.py
@@ -9,7 +9,7 @@ shapes.remove("cap")
 shapes.remove("snowflake")
 shapes.remove("mug")
 colors=list(properties["colors"].keys())
-shapes=["cone","pyramid"]
+#shapes=["cone","pyramid"]
 for shape1 in shapes:
     for shape2 in shapes:
         if shape1==shape2:
@@ -23,7 +23,7 @@ for shape1 in shapes:
                 arg_scales="1 1"
                 command="../blender-4.1.1-linux-x64/blender --background  --python render_images_custom.py -- --use_gpu 1  --objects %s\
                     --cobjects %s --robjects 0 0 --sobjects %s --objs_config data/TwoObjectSameColor.json  --num_images 1\
-                    --margin 0.01 --width 1024 --height 1024 --output_image_dir ../output/Two-Object-Diff-Color-Same-Size/" % (arg_shapes,arg_colors,arg_scales)
+                    --margin 0.01 --width 512 --height 512 --output_image_dir ../output/Two-Object-Diff-Color-Same-Size/" % (arg_shapes,arg_colors,arg_scales)
                 command=list(filter(None,command.split(" ")))
                 new_command=command[:5]
                 new_command=" ".join(command)
