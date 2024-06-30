0f3fd4cb0d547a5bf22a914a549b7d08f2840694
Aminreza Sefid <aminreza_sefid@yahoo.com>
2024-06-30 11:46:05 +0330

    three objects edited

--- image_generation/CoCo-1Object_Generation.py
+++ image_generation/CoCo-1Object_Generation.py
@@ -5,13 +5,24 @@ import json
 with open("objaverse_data/properties.json", 'r') as f:
     properties = json.load(f)
 shapes=list(properties["shapes"].keys())
-shapes=["zebra"]
+flat_shapes=["axe","banana","bed","clock","donut","fork","keyboard",
+"knife","sink","mouse","skateboard","remote","scissors",
+"pizza","plate","toilet","spoon","ski","snowboard"]
+lean_shapes=["cup","hat","laptop","suitcase"]
+shapes=["zebra","cup","mouse"]
 for shape in shapes:
     arg_shapes=shape
     arg_scales="1.3 1.3"
+    rotate1="270,0,0"
+    
+    rotate1="270,0,0"
+    if shape in flat_shapes:
+        rotate1="0,0,0"
+    if shape in lean_shapes:
+        rotate1="300,0,0"
     command="../blender-4.1.1-linux-x64/blender --background  --python render_objaverse.py -- --use_gpu 1  --objects %s\
-        --robjects 0,0,0 --sobjects %s --objs_config objaverse_data/OneObject.json \
-        --width 1024 --height 1024 --output_image_dir ../output/CoCo-OneObject/" % (arg_shapes,arg_scales)
+        --robjects %s --sobjects %s --objs_config objaverse_data/OneObject.json \
+        --width 512 --height 512 --cam_x 0 --cam_y 0 --cam_z 12 --output_image_dir ../output/CoCo-OneObject/" % (arg_shapes,rotate1,arg_scales)
     command=list(filter(None,command.split(" ")))
     new_command=command[:5]
     new_command=" ".join(command)
--- image_generation/CoCo-3Object_Generation.py
+++ image_generation/CoCo-3Object_Generation.py
@@ -7,7 +7,11 @@ with open("objaverse_data/properties.json", 'r') as f:
 with open("CoCo_Configs/Scales.json") as f:
     scales=json.load(f)
 shapes=list(properties["shapes"].keys())
-shapes=["zebra","boat","car"]
+shapes=["cup","clock","banana"]
+flat_shapes=["axe","banana","bed","clock","donut","fork","keyboard",
+"knife","sink","mouse","skateboard","remote","scissors",
+"pizza","plate","toilet","spoon","ski","snowboard"]
+lean_shapes=["cup","hat","laptop","suitcase"]
 for shape1 in shapes:
     for shape2 in shapes:
         for shape3 in shapes:
@@ -17,6 +21,26 @@ for shape1 in shapes:
             scale1=1.3
             scale2=1.3
             scale3=1.3
+            
+            rotate1="270,0,0"
+            rotate2="270,0,0"
+            rotate3="270,0,0"
+            if shape1 in flat_shapes:
+                rotate1="0,0,0"
+            if shape2 in flat_shapes:
+                rotate2="0,0,0"
+            if shape3 in flat_shapes:
+                rotate3="0,0,0"
+            
+            if shape1 in lean_shapes:
+                rotate1="300,0,0"
+            if shape2 in lean_shapes:
+                rotate2="300,0,0"
+            if shape3 in lean_shapes:
+                rotate3="300,0,0"
+
+
+
             if shape1 in scales.keys():
                 scale1*=scales[shape1]
             if shape2 in scales.keys():
@@ -25,8 +49,8 @@ for shape1 in shapes:
                 scale3*=scales[shape3]
             arg_scales="%f %f %f" % (scale1,scale2,scale3)
             command="../blender-4.1.1-linux-x64/blender --background  --python render_objaverse.py -- --use_gpu 1  --objects %s\
-                --robjects 0,0,0 0,0,0 0,0,0 --sobjects %s --objs_config CoCo_Configs/ThreeObjectSameSize.json \
-                --width 512 --height 512 --output_image_dir ../output/CoCo-ThreeObjectSameSize/" % (arg_shapes,arg_scales)
+                --robjects %s %s %s --sobjects %s --objs_config CoCo_Configs/ThreeObjectSameSize.json \
+                --width 512 --height 512 --cam_x 0 --cam_y 0 --cam_z 12 --output_image_dir ../output/CoCo-ThreeObjectSameSize/" % (arg_shapes,rotate1,rotate2,rotate3,arg_scales)
             command=list(filter(None,command.split(" ")))
             new_command=command[:5]
             new_command=" ".join(command)
